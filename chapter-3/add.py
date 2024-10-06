6aa86113ad3312fb31206eff5a171ce5e40f9775
pibragimov <pibragimov@whoosh.bike>
2024-10-07 00:51:23 +0300

    auto: autocommit

---
+++ chapter-3/add.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    """Returns sum of a and b
+
+    Args:
+        a (int): first argument
+        b (int): secont argument
+
+    Returns:
+        int: sum
+    """
+    return a + b
---
+++ chapter-3/main.py
@@ -0,0 +1,9 @@
+from add import add
+
+
+def main():
+    print(add(1, 2))
+
+
+if __name__ == "__main__":
+    main()
