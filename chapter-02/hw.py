13ca3fa834afef453f1ad63ae0e8fd2cc9f789bf
Peter Ibragimov <pibragimov@mts.ru>
2025-10-14 16:17:48 +0700

    auto: autocommit

---
+++ chapter-02/hw.py
@@ -0,0 +1,22 @@
+import time
+import random
+
+
+def timeit(name: str, iters: int) -> None:
+    results = []
+    for _ in range(iters):
+        list_ = list(range(10_000))
+        item = random.randint(0, 10_000)
+
+        t0 = time.monotonic_ns()
+
+        list_.index(item)
+
+        results.append(time.monotonic_ns() - t0)
+
+    avg_dur = (sum(results) / len(results)) / 1_000
+
+    print(f"{name}: {avg_dur:.2f}ms")
+
+
+timeit("Find in list", 1000)
