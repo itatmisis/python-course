05ce366a392148021cf9121b9e31f616fb6ddc96
Peter Ibragimov <pibragimov@mts.ru>
2025-11-05 14:29:59 +0400

    daily minor edit from 0000NBB04VMQ0XM

---
+++ chapter-09/main.py
@@ -0,0 +1,9 @@
+addresses = [
+    "https://misis.ru",
+    "https://edu.itatmisis.ru/",
+    "https://ya.ru",
+    "https://google.com",
+    "https://python.org",
+    "https://go.dev",
+    "https://rust-lang.org/",
+]
